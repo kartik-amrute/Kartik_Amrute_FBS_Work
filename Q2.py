ef04f442470bd7fc8d57ab789cc7dd02ab321bb9
kartik-amrute <kartikamrute949@gmail.com>
2025-07-02 11:43:45 -0500

    Delete Q2.py

--- Q2.py
+++
@@ -1,7 +0,0 @@
-pri=int(input("enter the principal amount of: "))
-rate=float(input("enter the rate of: " ))
-time=int(input("enter the time of year:" ))
-
-simple_interest=(pri*rate*time)/100
-
-print("simple_interest",simple_interest)
