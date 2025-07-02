749069ac5f5c77ddb80da217d0649153436cc424
kartik-amrute <kartikamrute949@gmail.com>
2025-07-02 11:43:22 -0500

    Delete Q1.py

--- Q1.py
+++
@@ -1,9 +0,0 @@
-length=int(input("enter the length of: "))
-breadth=int(input("enter the breadth of: "))
-radius=int(input("enter the redius of: "))
-
-area=length*breadth
-perimeter=area**2
-
-print("area",area)
-print("perimeter",perimeter)
