72277445264939b53ae95952a01e4d59e22c5f9d
kartik-amrute <kartikamrute949@gmail.com>
2025-07-02 11:44:17 -0500

    Delete Q4.py

--- Q4.py
+++
@@ -1,9 +0,0 @@
-side=int(input("enter the side: "))
-costint=int(input("enter the cost of intereal: "))
-costext=int(input("enter the cost of extereal: "))
-
-area=4*side *side
-
-totalcost=area*costint+area*costext
-
-print("total cost of panting 2 room : ",2*totalcost)
