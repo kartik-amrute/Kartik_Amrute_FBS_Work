ff8fa369aab3a6752f388e89a5f6a2dcdc1a013a
kartik-amrute <kartikamrute949@gmail.com>
2025-07-02 22:04:26 +0530

    Test 1

---
+++ Q1.py
@@ -0,0 +1,9 @@
+length=int(input("enter the length of: "))
+breadth=int(input("enter the breadth of: "))
+radius=int(input("enter the redius of: "))
+
+area=length*breadth
+perimeter=area**2
+
+print("area",area)
+print("perimeter",perimeter)
